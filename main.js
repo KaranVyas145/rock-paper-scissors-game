95eda95a05b27fecbd203413ebf08da75b20d0ba
Karan Vyas <karanvyas2907@gmail.com>
2022-01-19 14:02:50 +0530

    added game over

--- main.js
+++ main.js
@@ -7,18 +7,23 @@ const game = () => {
   const paperbtn = document.getElementById("paperbtn");
   const scissorbtn = document.getElementById("scissorbtn");
 
-  const playerChance = [rockbtn, paperbtn, scissorbtn];
+  const playerChanceAll = [rockbtn, paperbtn, scissorbtn];
 
-  playerChance.forEach((element) => {
+  playerChanceAll.forEach((element) => {
     element.addEventListener("click", function () {
       moves++;
-      console.log("Moves left " + 10 - moves);
+    //   console.log("Moves left "+(10-moves));
       const computerChance = computerPlay();
       document.getElementById('computer-choice').innerHTML=computerChance;
       document.getElementById('player-choice').innerHTML=element.innerText;
-      document.getElementById('winner').innerHTML=`Moves left ${10-moves}`;
+      document.getElementById('moves').innerHTML=`Moves left ${10-moves}`;
+      const gameWinner=winner(element.innerText,computerChance);
+    //   console.log(gameWinner);
       if(moves===10){
-        //   document.getElementById('play-game').style.display="none";
+          document.getElementById('play-game').style.display="none";
+          document.getElementById('moves').style.display="none";
+        //   document.getElementById('winner').style.display="none"
+          gameOver(gameWinner);
         //Game over
       }
     });
@@ -34,6 +39,65 @@ const game = () => {
       case 3:
         return "PAPER";
     }
+  }
+
+  function winner(playerChance,computerChance){
+    // console.log(playerChance);
+    // console.log(computerChance);
+    if(computerChance===playerChance){
+        document.getElementById('winner').innerHTML="Its a tie!!!";
+    }
+    else if(computerChance==="ROCK"){
+        if(playerChance==="SCISSORS"){
+            document.getElementById('winner').innerHTML="Computer Won";
+            computerScore++;
+            document.getElementById('computer-score').innerHTML=computerScore;
+        }
+        else{
+            // console.log("player won");
+            playerScore++;
+            document.getElementById('player-score').innerHTML=playerScore;
+            document.getElementById('winner').innerHTML="You Won";
+        }
+    }
+    else if(computerChance==="SCISSORS"){
+        if(playerChance==="PAPER"){
+            document.getElementById('winner').innerHTML="Computer Won";
+            computerScore++;
+            document.getElementById('computer-score').innerHTML=computerScore;
+        }
+        else{
+            // console.log("player won");
+            playerScore++;
+            document.getElementById('player-score').innerHTML=playerScore;
+            document.getElementById('winner').innerHTML="You Won";
+        }
+    }
+    else if(computerChance==="PAPER"){
+        if(playerChance==="ROCK"){
+            document.getElementById('winner').innerHTML="Computer Won";
+            computerScore++;
+            document.getElementById('computer-score').innerHTML=computerScore;
+        }
+        else{
+            // console.log("player won");
+            playerScore++;
+            document.getElementById('player-score').innerHTML=playerScore;
+            document.getElementById('winner').innerHTML="You Won";
+        }
+    }
+
+    return playerScore>computerScore? "YOU" : "Computer";
+  }
+
+  function gameOver(winner){
+      document.getElementById('winner').innerHTML=`${winner} won this game <br> <button id="restart">Restart Game</button>`;
+      document.getElementById('winner').classList='game-over';
+
+      const restart=document.getElementById('restart');
+      restart.addEventListener('click',()=>{
+        window.location.reload();
+      })
   }
 };
 
